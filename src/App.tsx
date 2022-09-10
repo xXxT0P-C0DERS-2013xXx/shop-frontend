e2c4d0978664be449752b85d2389d5769da94d63
romchesko-pazzi <romchesko.pazzi@gmail.com>
2022-09-10 11:32:39 +0300

    refactor: minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { Header } from './components/Header/Header';
+import { Home } from './components/Home/Home';
 
 export const App = () => {
   return (
     <div>
-      <Header />
+      <Home />
     </div>
   );
 };
